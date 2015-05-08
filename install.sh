4f2b2fef58b9932e9314d3c925635f5619e024ad
Icer <liangshuangde@163.com>
2015-05-09 06:12:44 +0800

    update script

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@
 # git clone https://github.com/pixel-cookers/redmine-theme.git $theme/pixel-cookers
 # git clone https://github.com/Nitrino/flatly_light_redmine.git $theme/flatly_light
 
-cat /etc/network/interfaces | sed "s/iface eth0 inet dhcp/iface eth0 inet static\naddress 192.168.1.159\nnetmask 255.255.255.0\ngateway 192.168.1.111/">/etc/network/interfaces
+sed -i.bak "s/iface eth0 inet dhcp/iface eth0 inet static\naddress 192.168.1.159\nnetmask 255.255.255.0\ngateway 192.168.1.111/" /etc/network/interfaces
