3c6ed86c71414e61021aab81f369c12846506f1d
Icer <liangshuangde@163.com>
2015-05-09 06:22:13 +0800

    update script

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #! /bin/sh
-# /opt/bitnami/apps/wordpress/bnconfig --disable_banner 1
+
+/opt/bitnami/apps/wordpress/bnconfig --disable_banner 1
 # /opt/bitnami/ctlscript.sh restart apache
 
-# themes=/opt/bitnami/apps/redmine/htdocs/public/themes
-# git clone https://github.com/akabekobeko/redmine-theme-minimalflat.git $theme/minimalflat
-# git clone https://github.com/pixel-cookers/redmine-theme.git $theme/pixel-cookers
-# git clone https://github.com/Nitrino/flatly_light_redmine.git $theme/flatly_light
+themes=/opt/bitnami/apps/redmine/htdocs/public/themes
+git clone https://github.com/akabekobeko/redmine-theme-minimalflat.git $theme/minimalflat
+git clone https://github.com/pixel-cookers/redmine-theme.git $theme/pixel-cookers
+git clone https://github.com/Nitrino/flatly_light_redmine.git $theme/flatly_light
+
+mv /etc/init/ssh.conf.back /etc/init/ssh.conf
+start ssh
 
 sed -i.bak "s/iface eth0 inet dhcp/iface eth0 inet static\naddress 192.168.1.159\nnetmask 255.255.255.0\ngateway 192.168.1.111/" /etc/network/interfaces
