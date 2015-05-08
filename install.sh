adc336be09020f68f2e7573338890a138890ed20
Icer <liangshuangde@163.com>
2015-05-09 06:03:18 +0800

    basic commands

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #! /bin/sh
-/opt/bitnami/apps/wordpress/bnconfig --disable_banner 1
-/opt/bitnami/ctlscript.sh restart apache
+# /opt/bitnami/apps/wordpress/bnconfig --disable_banner 1
+# /opt/bitnami/ctlscript.sh restart apache
 
+# themes=/opt/bitnami/apps/redmine/htdocs/public/themes
+# git clone https://github.com/akabekobeko/redmine-theme-minimalflat.git $theme/minimalflat
+# git clone https://github.com/pixel-cookers/redmine-theme.git $theme/pixel-cookers
+# git clone https://github.com/Nitrino/flatly_light_redmine.git $theme/flatly_light
+
+cat /etc/network/interfaces | sed "s/iface eth0 inet dhcp/iface eth0 inet static\naddress 192.168.1.159\nnetmask 255.255.255.0\ngateway 192.168.1.111/">/etc/network/interfaces
